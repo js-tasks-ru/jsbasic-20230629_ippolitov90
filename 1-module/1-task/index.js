c7796c26d8ede73119806908c95188da8ed15d90
ozakipwnz <ippolitov90@yandex.ru>
2023-07-06 21:48:11 +0800

    update

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,13 +1,11 @@
-alert(factorial(0));
-alert(factorial(1));
-alert(factorial(3));
-alert(factorial(5));
 function factorial(n) { 
-      let b = 1; 
-      
-    if(n === 0)
-        return 1;
-    for (let i = 1; i <= n; i++) 
-        b = b * i;  
-  return b;
-}
+       let b = 1; 
+       if(n >=  0)
+       for (let i = 1; i <= n; i++) 
+         b = b * i;  
+   alert(b);
+ }
+ factorial(0);
+ factorial(1);
+ factorial(3);
+ factorial(5);
