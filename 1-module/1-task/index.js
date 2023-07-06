ea511d21d687d5e3a38fa11482ca11aad2010172
ozakipwnz <ippolitov90@yandex.ru>
2023-07-06 20:53:02 +0800

    update

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,13 @@
-function factorial(n) {
-  // ваш код...
+alert(factorial(0));
+alert(factorial(1));
+alert(factorial(3));
+alert(factorial(5));
+function factorial(n) { 
+      let b = 1; 
+      
+    if(n === 0)
+        return 1;
+    for (let i = 1; i <= n; i++) 
+        b = b * i;  
+  return b;
 }
