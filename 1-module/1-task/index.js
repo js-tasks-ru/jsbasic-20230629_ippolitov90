030176b9c9aabac8d9400e975f9336bb5e1a1a94
ozakipwnz <ippolitov90@yandex.ru>
2023-07-06 23:42:01 +0800

    correction

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,11 +1,11 @@
 function factorial(n) { 
-       let b = 1; 
-       if(n >=  0)
-       for (let i = 1; i <= n; i++) 
-         b = b * i;  
-   alert(b);
- }
- factorial(0);
- factorial(1);
- factorial(3);
- factorial(5);
+  let b = 1; 
+  if(n >=  0)
+  for (let i = 1; i <= n; i++) 
+    b = b * i;
+return (b);
+}
+factorial(0);
+factorial(1);
+factorial(3);
+factorial(5);
